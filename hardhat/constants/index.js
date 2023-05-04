dd38bd7642aac8176273a98ccbb41768340c8509
srb1998 <svgupta@mitaoe.ac.in>
2023-05-04 14:47:46 +0530

    update metadata URL

--- hardhat/constants/index.js
+++ hardhat/constants/index.js
@@ -1,6 +1,6 @@
 
 const WHITELIST_CONTRACT_ADDRESS = "0x51D4D6DcdcfBa5d6De431dadc3010b8c9ebda1cc";
 
-const METADATA_URL = "";
+const METADATA_URL = "https://spiderverse-nft.vercel.app/api/";
 
-module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL}; 
+module.exports = {WHITELIST_CONTRACT_ADDRESS,METADATA_URL};
