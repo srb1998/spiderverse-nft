47d6e0a8b91e6f603a6f9b235d4c24cf7b76f236
srb1998 <svgupta@mitaoe.ac.in>
2023-05-04 14:37:35 +0530

    Added API to load Metadata of NFTs

--- my-app/pages/api/[tokenId].js
+++ my-app/pages/api/[tokenId].js
@@ -3,8 +3,16 @@
 export default function handler(req, res) {
   
     const tokenId = req.query.tokenId;
-    res.status(200).json({
-        tokenId:tokenId,
+
+    const name = `SpiderVerse #${tokenId}`;
+    const description = "This is your Ticket to SpiderVerse";
+    const image = `https://raw.githubusercontent.com/srb1998/spiderverse-nft/main/my-app/public/spiderverse/${tokenId}.svg`;
+
+
+    return res.json({
+        name:name,
+        description: description,
+        image: image,
     });
 
 }
